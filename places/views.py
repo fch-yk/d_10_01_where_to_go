d3c91409858160cccc58397fefa92f4c3b0eb676
yk <yefim_korshever@mail.ru>
2023-01-13 19:14:40 +0300

    Get GeoJSON from the database

--- places/views.py
+++ places/views.py
@@ -15,39 +15,17 @@ def home(request):
             "properties": {
                 "title": place.title,
                 "placeId": place.id,
-                "detailsUrl": "./static/places/moscow_legends.json"
+                "detailsUrl": "./static/places/moscow_legends.json",  # FIXME
             }
         }
         features.append(feature)
 
+    # FIXME debug
+    features[1]["properties"]["detailsUrl"] = "./static/places/roofs24.json"
+
     places = {
         "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
-                },
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "./static/places/moscow_legends.json"
-                }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "./static/places/roofs24.json"
-                }
-            }
-        ]
+        "features": features,
     }
 
     context = {"places": places}
