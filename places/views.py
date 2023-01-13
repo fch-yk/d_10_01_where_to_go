74ecb45fcd39ea96e347bd8fdeee07a6f442c417
yk <yefim_korshever@mail.ru>
2023-01-13 13:39:39 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,6 +1,54 @@
 from django.shortcuts import render
+from .models import Place
 
 
 def home(request):
-    context = {}
+
+    features = []
+    for place in Place.objects.all():
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.lng, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": "./static/places/moscow_legends.json"
+            }
+        }
+        features.append(feature)
+
+    places = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.62, 55.793676]
+                },
+                "properties": {
+                    "title": "«Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": "./static/places/moscow_legends.json"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.64, 55.753676]
+                },
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": "./static/places/roofs24.json"
+                }
+            }
+        ]
+    }
+
+    context = {"places": places}
     return render(request, 'index.html', context=context)
