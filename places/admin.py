107f8af18d729bbca133566d7be5eee3d1f6bbfd
yk <yefim_korshever@mail.ru>
2023-01-15 08:31:58 +0300

    Let add a photo from a place card in admin site

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import Place, Photo
 
 
+class PhotosInline(admin.TabularInline):
+    model = Photo
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     readonly_fields = ("id",)
+    inlines = [PhotosInline,]
 
 
 @admin.register(Photo)
