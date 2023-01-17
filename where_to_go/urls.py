18536084ce40d77b63b548ab823d13ae2ad440d9
yk <yefim_korshever@mail.ru>
2023-01-17 21:18:27 +0300

    Change staic root

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', include('places.urls')),
     path('tinymce/', include('tinymce.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
